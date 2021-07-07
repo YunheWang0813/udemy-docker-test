2350aa0569961b4b9d30499d8023ca86a06c34af
yw314 <yw314@duke.edu>
2021-07-08 01:43:32 +0900

    branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There!
+          Change!
         </p>
         <a
           className="App-link"
